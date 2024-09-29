6a501044a58c9278b184237a1b0e8cef05735e0e
olml89 <oleguer.ml89@gmail.com>
2024-09-29 14:00:14 +0200

    testing: Optimize memory consumption

--- app/PrimeNumbers/EratosthenesPrimeNumbersCalculator.php
+++ app/PrimeNumbers/EratosthenesPrimeNumbersCalculator.php
@@ -8,13 +8,15 @@ final class EratosthenesPrimeNumbersCalculator implements PrimeNumbersCalculator
 {
     public function calculate(int $number): array
     {
+        $limit = (int)sqrt($number);
+
         $primeNumbersHash = [
             0 => false,
             1 => false,
             ...array_fill(2, max($number - 1, 0), true),
         ];
 
-        for ($i = 2; $i <= $number; $i++) {
+        for ($i = 2; $i <= $limit; $i++) {
             if ($primeNumbersHash[$i]) {
                 for ($j = $i * $i; $j <= $number; $j += $i) {
                     $primeNumbersHash[$j] = false;
